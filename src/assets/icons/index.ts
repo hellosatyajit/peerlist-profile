7aaf2d3629605c2e5b5fb844797f463cbb7017e8
Satyajit Chaudhary <thisissatyajit05@gmail.com>
2025-02-21 23:50:00 +0530

    project init

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+import TwitterIcon from "./TwitterIcon.astro";
+import ProductHuntIcon from "./ProductHunt.astro";
+import LinkedInIcon from "./LinkedinIcon.astro";
+import InstagramIcon from "./InstagramIcon.astro";
+import PeerlistIcon from "./PeerlistIcon.astro";
+
+export { TwitterIcon, ProductHuntIcon, LinkedInIcon, InstagramIcon, PeerlistIcon };
---
+++ src/utils/peerlist.ts
@@ -0,0 +1,230 @@
+export interface PeerlistData {
+  follower: boolean;
+  following: boolean;
+  peer: boolean;
+  isPeers: boolean;
+  certificationsCount: number;
+  communityWorkCount: number;
+  companyVerifiedAt: string;
+  displayName: string;
+  educationCount: number;
+  collectionsCount: number;
+  enabled: boolean;
+  experienceCount: number;
+  projectCount: number;
+  firstName: string;
+  headline: string;
+  address: {
+    country: string;
+    city: string;
+    countryCode: string;
+  };
+  inboxPreferences: null;
+  integrations: {
+    productHunt?: { username: string };
+    github?: { username: string; verified: boolean };
+  };
+  integrationsOrder: Array<{ name: string }>;
+  lastName: string;
+  preferredPronoun: string;
+  profileHandle: string;
+  profilePicture: string;
+  projectsOrder: string[];
+  published: boolean;
+  skills: Array<{
+    id?: string;
+    name: string;
+    label: string;
+  }>;
+  socialLinks: {
+    Twitter?: string;
+    Instagram?: string;
+    LinkedIn?: string;
+    ProductHunt?: string;
+  };
+  totalExperienceYears: number;
+  verified: boolean;
+  website?: string;
+  calendar: string;
+  createdAt: string;
+  publishedAt: string;
+  id: string;
+  projects: Array<{
+    title: string;
+    slug: string;
+    tagline?: string;
+    url?: string;
+    tags: string[];
+    categories?: Array<{
+      name: string;
+      id: string;
+      title: string;
+      description: string;
+    }>;
+    images?: Array<string>;
+    isOpenSource?: boolean;
+    createdAt: string;
+    hackathonInfo: {
+      slug: string;
+      submittedAt: null;
+    } | null;
+    openForCollab: boolean;
+    commentsUpdatedAt: string;
+    id: string;
+    teamName?: string;
+    createdBy: {
+      displayName: string;
+      firstName: string;
+      headline: string;
+      lastName: string;
+      profileHandle: string;
+      profilePicture: string;
+      companyVerifiedAt: string;
+      verified: boolean;
+      id: string;
+      canSuperUpvote: boolean;
+    };
+    creator: {
+      displayName: string;
+      firstName: string;
+      headline: string;
+      lastName: string;
+      profileHandle: string;
+      profilePicture: string;
+      companyVerifiedAt: string;
+      verified: boolean;
+      id: string;
+      canSuperUpvote: boolean;
+    };
+    projectURL: string;
+    collaborators: Array<{
+      firstName: string;
+      lastName: string;
+      displayName: string;
+      profilePicture: string;
+      headline: string;
+      profileHandle: string;
+      id: string;
+    }>;
+    upvotesCount?: number;
+    isUpvoted?: boolean;
+    commentCount?: number;
+    bookmarkCount?: number;
+  }>;
+  education: Array<{
+    createdAt: string;
+    title: string;
+    startDate: string;
+    endDate: string;
+    organizationName: string;
+    courseName?: string;
+    skills: Array<{ name: string; label: string }>;
+    current: boolean;
+    promoted: boolean;
+    verifiedAt: null | string;
+    uuid: string;
+    isExpiring: boolean;
+    verified: boolean;
+    id: string;
+    instituteName: string;
+    institution: string;
+    degree: string;
+    major?: string;
+    startYear: number;
+    endYear: number;
+  }>;
+  experience: Array<{
+    createdAt: string;
+    title: string;
+    position: string;
+    description: string;
+    startDate: string;
+    endDate: string;
+    location: {
+      city?: string;
+      country?: string;
+      code?: string;
+    } & ({ city: string } | { country: string });
+    organizationName: string;
+    jobType: string;
+    skills: Array<{
+      id?: string;
+      name: string;
+      label: string;
+    }>;
+    current: boolean;
+    promoted: boolean;
+    verifiedAt: null | string;
+    website?: string;
+    uuid: string;
+    isExpiring: boolean;
+    verified: boolean;
+    role: string;
+    id: string;
+    companyId?: string;
+    company: string;
+    companyName?: string;
+    logo?: string;
+    profileHandle?: string;
+    isClaimed?: boolean;
+    tagline?: string;
+    companyVerifiedAt?: string;
+    companyVerifiedBy?: string;
+    companyVerification?: {
+      approvedBy: {
+        at: string;
+        id: string;
+      };
+    };
+    workSkills: Array<{
+      id?: string;
+      name: string;
+      label: string;
+    }>;
+    jobRole: string;
+  }>;
+  numPosts: number;
+  blogCount: number;
+  jobsCount: number;
+  networkCount: {
+    followers: number;
+    peers: number;
+  };
+  ogDetails: {
+    title: string;
+    description: string;
+    tags: string[];
+  };
+}
+
+async function fetchFromPeerlist(url: string): Promise<{ user: PeerlistData }> {
+  const response = await fetch(`https://peerlist.io/${url}`);
+  const html = await response.text();
+  const scriptDataMatch = html.match(
+    /<script id="__NEXT_DATA__" type="application\/json">(.*?)<\/script>/s
+  );
+  if (!scriptDataMatch) {
+    throw new Error("Could not find Peerlist data in HTML");
+  }
+  const jsonData = JSON.parse(scriptDataMatch[1]);
+  
+  jsonData.props.pageProps.user.address = {
+    country: "India",
+    city: "Ahmedabad",
+    countryCode: "IN",
+  };
+  return jsonData.props.pageProps;
+}
+
+export async function fetchUserProfile(
+  username: string
+): Promise<PeerlistData> {
+  const jsonData = await fetchFromPeerlist(username);
+  return jsonData.user;
+}
+
+export async function fetchUserResume(username: string): Promise<PeerlistData> {
+  const jsonData = await fetchFromPeerlist(`${username}/resume`);
+
+  return jsonData.user;
+}
