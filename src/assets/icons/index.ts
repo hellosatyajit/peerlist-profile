8a4fb3c8f50f4bf90b6366d885167762b9ccc836
Satyajit Chaudhary <thisissatyajit05@gmail.com>
2025-02-26 19:08:44 +0530

    add more icons and improve UI consistency and fixes

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,5 +3,17 @@ import ProductHuntIcon from "./ProductHunt.astro";
 import LinkedInIcon from "./LinkedinIcon.astro";
 import InstagramIcon from "./InstagramIcon.astro";
 import PeerlistIcon from "./PeerlistIcon.astro";
+import FigmaIcon from "./FigmaIcon.astro";
+import ThreadsIcon from "./ThreadsIcon.astro";
+import YoutubeIcon from "./YoutubeIcon.astro";
 
-export { TwitterIcon, ProductHuntIcon, LinkedInIcon, InstagramIcon, PeerlistIcon };
+export {
+  TwitterIcon,
+  ProductHuntIcon,
+  LinkedInIcon,
+  InstagramIcon,
+  PeerlistIcon,
+  FigmaIcon,
+  ThreadsIcon,
+  YoutubeIcon,
+};
